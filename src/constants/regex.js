bda48e31f69294eebefdf30056080dcb0880ada0
flangham <finnian.langham@gmail.com>
2022-10-14 11:01:10 +1100

    Updated regexes

--- src/constants/regex.js
+++ src/constants/regex.js
@@ -1,8 +1,11 @@
+const basicText = /^[\s]*[\S]+[\s\S]*$/; // At least 1 non-whitespace character, leading + trailing whitespace is allowed
+const email = /^\s*\S+[@]\S+[.]\S+\s*$/; // <love>@<money>.<beta>, leading + trailing whitespace is allowed
+
 const regex = {
-  name: /^.+$/, // Anything
-  email: /^.+[@].+[.].+$/, // <love>@<money>.<beta>
-  enquiry: /^.+$/, // Anything
-  message: /^.+$/ // Anything
+  name: basicText,
+  email,
+  enquiry: basicText,
+  message: basicText
 };
 
 export default regex;
