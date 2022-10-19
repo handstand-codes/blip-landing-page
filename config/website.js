c1151ea901c790597a315d6b2a00eedd0cd22a54
flangham <finnian.langham@gmail.com>
2022-10-19 13:56:45 +1100

    Update metadata

--- config/website.js
+++ config/website.js
@@ -1,14 +1,14 @@
 module.exports = {
   pathPrefix: `/`, // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: `LAM Landingpage Template`, // Default Site Title used for SEO & PWA
-  description: `LAM Landingpage Template`, // Default Site Decription used for SEO
-  siteName: `LAM Landingpage Template`, // Sitename for Facebook
+  title: `Blip`, // Default Site Title used for SEO & PWA
+  description: `Blip description goes here`, // Default Site Decription used for SEO
+  siteName: `Blip`, // Sitename for Facebook
   banner: `/open-graph.jpg`, // Default OpenGraph image
   ogLanguage: `en_AU`, // Facebook Language
   socialLinks: [``], // Array of social links (facebook, insta, etc)
   // JSONLD / Manifest
   icon: `/favicon-light-mode.png`, // Used for manifest favicon, splash screen, and icon generation
-  shortName: `Landingpage`, // shortname for manifest. MUST be shorter than 12 characters
+  shortName: `Blip`, // shortname for manifest. MUST be shorter than 12 characters
   author: `Love + Money`, // Author for schemaORGJSONLD
   themeColor: `#000000`, // PWA Icon background & address bar color if installed on desktop
   backgroundColor: `#000000`, // PWA color shown before styles and content loads, should match the background-color CSS property in the site’s stylesheet
