8385c45e422095e665bd12562da4ba6ead58fe41
flangham <finnian.langham@gmail.com>
2022-10-19 15:09:28 +1100

    Setup new fonts + styles

--- src/components/Theme/Fonts/index.jsx
+++ src/components/Theme/Fonts/index.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { css, Global } from "@emotion/react";
-import ppNeueMontrealBoldWOFF from "~assets/fonts/pp-neue-montreal-bold.woff";
-import ppNeueMontrealBoldWOFF2 from "~assets/fonts/pp-neue-montreal-bold.woff2";
-import ppNeueMontrealMediumWOFF from "~assets/fonts/pp-neue-montreal-medium.woff";
-import ppNeueMontrealMediumWOFF2 from "~assets/fonts/pp-neue-montreal-medium.woff2";
+
+import ArchivoSemiBoldWOFF from "~assets/fonts/Archivo-SemiBold.woff";
+import ArchivoSemiBoldWOFF2 from "~assets/fonts/Archivo-SemiBold.woff2";
+import ArchivoSemiBoldItalicWOFF from "~assets/fonts/Archivo-SemiBoldItalic.woff";
+import ArchivoSemiBoldItalicWOFF2 from "~assets/fonts/Archivo-SemiBoldItalic.woff2";
+
 import * as bp from "~styles/breakpoints.module.scss";
 
 const SANS_FALLBACKS = `"Helvetica Neue", "Helvetica", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`;
 
-const PP_NEUE_MONTREAL_ID = `PP Neue Montreal`;
-const PP_NEUE_MONTREAL_TEXT_GROUP = `"${PP_NEUE_MONTREAL_ID}", ${SANS_FALLBACKS}`;
+const ARCHIVO_ID = `Archivo`;
+const ARCHIVO_TEXT_GROUP = `"${ARCHIVO_ID}", ${SANS_FALLBACKS}`;
 
 /** ============================================================================
  * @component
@@ -19,21 +21,22 @@ const Fonts = () => (
   <Global
     styles={css`
       @font-face {
-        font-family: "${PP_NEUE_MONTREAL_ID}";
-        src: url(${ppNeueMontrealMediumWOFF2}) format("woff2"),
-          url(${ppNeueMontrealMediumWOFF}) format("woff");
+        font-family: "${ARCHIVO_ID}";
+        src: url(${ArchivoSemiBoldWOFF}) format("woff2"),
+          url(${ArchivoSemiBoldWOFF2}) format("woff");
         font-display: block;
         font-weight: normal;
         font-style: normal;
       }
 
       @font-face {
-        font-family: "${PP_NEUE_MONTREAL_ID}";
-        src: url(${ppNeueMontrealBoldWOFF2}) format("woff2"),
-          url(${ppNeueMontrealBoldWOFF}) format("woff");
+        font-family: "${ARCHIVO_ID}";
+        src: url(${ArchivoSemiBoldItalicWOFF}) format("woff2"),
+          url(${ArchivoSemiBoldItalicWOFF2}) format("woff");
         font-display: block;
-        font-weight: 900;
+        font-weight: normal;
         font-style: normal;
+        font-style: italic;
       }
 
       // common //
@@ -48,155 +51,135 @@ const Fonts = () => (
       .b2,
       .button-text,
       .caption {
-        font-family: ${PP_NEUE_MONTREAL_TEXT_GROUP};
+        font-family: ${ARCHIVO_TEXT_GROUP};
         font-weight: normal;
       }
 
       // display //
       .d1 {
         font-weight: 700;
-        font-size: 48px;
+        font-size: 42px;
         line-height: 110%;
-        letter-spacing: -0.0125em;
-        text-transform: uppercase;
-      }
-
-      .d2 {
-        font-weight: 700;
-        font-size: 36px;
-        line-height: 110%;
-        letter-spacing: -0.0125em;
-        text-transform: uppercase;
+        letter-spacing: -0.02em;
       }
 
       // headings //
       .h1 {
         font-weight: 700;
-        font-size: 28px;
+        font-size: 26px;
         line-height: 110%;
-        letter-spacing: -0.005em;
-        text-transform: uppercase;
+        letter-spacing: -0.01em;
       }
 
       .h2 {
         font-weight: 700;
-        font-size: 24px;
-        line-height: 110%;
-        letter-spacing: -0.005em;
-        text-transform: uppercase;
+        font-size: 22px;
+        line-height: 120%;
+        letter-spacing: -0.01em;
       }
 
       .h3 {
         font-weight: 700;
-        font-size: 20px;
-        line-height: 110%;
-        letter-spacing: -0.005em;
-        text-transform: uppercase;
-      }
-
-      .h4 {
-        font-weight: 700;
         font-size: 18px;
-        line-height: 110%;
-        letter-spacing: -0.005em;
-        text-transform: uppercase;
+        line-height: 120%;
+        letter-spacing: -0.01em;
       }
 
       // body //
       .b1 {
-        font-weight: 500;
+        font-weight: 400;
         font-size: 18px;
-        line-height: 110%;
-        letter-spacing: 0.004em;
+        line-height: 120%;
       }
 
       .b2 {
-        font-weight: 500;
-        font-size: 14px;
-        line-height: 110%;
-        letter-spacing: 0.01em;
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 120%;
       }
 
       // other //
       .caption {
-        font-weight: 500;
-        font-size: 12px;
-        line-height: 110%;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 120%;
         letter-spacing: 0.02em;
-        /* font-feature-settings: "zero" on; */
       }
 
       .button-text {
         font-weight: 700;
+        font-size: 24px;
+        line-height: 120%;
+        letter-spacing: 0.01em;
+      }
+
+      .button-text-underline {
+        font-weight: 400;
         font-size: 16px;
         line-height: 120%;
         letter-spacing: 0.01em;
-        text-transform: uppercase;
       }
 
       @media only screen and (min-width: ${bp.smallTablet}) {
         // display //
         .d1 {
-          font-size: 80px;
-          line-height: 90%;
-          letter-spacing: -0.02em;
-        }
-
-        .d2 {
-          font-size: 62px;
-          line-height: 90%;
-          letter-spacing: -0.02em;
+          font-size: 56px;
         }
 
         // headings //
         .h1 {
-          font-size: 48px;
-          line-height: 110%;
-          letter-spacing: -0.02em;
+          font-size: 36px;
         }
 
         .h2 {
-          font-size: 35px;
-          line-height: 110%;
-          letter-spacing: -0.02em;
+          font-size: 26px;
         }
 
         .h3 {
-          font-size: 24px;
-          line-height: 110%;
-          letter-spacing: -0.02em;
-        }
-
-        .h4 {
-          font-size: 20px;
-          line-height: 110%;
-          letter-spacing: -0.02em;
+          font-size: 22px;
         }
 
         // body //
         .b1 {
-          font-size: 20px;
-          line-height: 110%;
-          letter-spacing: 0.01em;
+          font-size: 22px;
         }
 
         .b2 {
-          font-size: 16px;
-          line-height: 110%;
-          letter-spacing: 0.01em;
+          font-size: 14px;
         }
 
         // other //
         .caption {
-          font-size: 12px;
-          line-height: 120%;
-          letter-spacing: 0.02em;
+          font-size: 16px;
+        }
+      }
+
+      @media only screen and (min-width: ${bp.smallDesktop}) {
+        // display //
+        .d1 {
+          font-size: 64px;
         }
 
-        .button-text {
-          font-size: 20px;
-          line-height: 100%;
-          letter-spacing: 0.01em;
+        // headings //
+        .h1 {
+          font-size: 42px;
+        }
+
+        .h2 {
+          font-size: 32px;
+        }
+
+        .h3 {
+          font-size: 24px;
+        }
+
+        // body //
+        .b1 {
+          font-size: 24px;
+        }
+
+        .b2 {
+          font-size: 16px;
         }
       }
     `}
