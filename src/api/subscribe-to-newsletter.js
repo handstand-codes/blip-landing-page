3f2b861bb9106d61cb066d7f60e442633f6b5318
flangham <finnian.langham@gmail.com>
2022-10-20 13:10:43 +1100

    Integrated signup form with klaviyo

--- src/api/subscribe-to-newsletter.js
+++ src/api/subscribe-to-newsletter.js
@@ -5,7 +5,7 @@ const subscribeToNewsletter = async (req, res) => {
     return res.status(500).json({ error: `Invalid data` });
   }
   try {
-    const NEWSLETTER_LIST_ID = `YqZLti`;
+    const NEWSLETTER_LIST_ID = `UYUAEG`;
     const url = `https://a.klaviyo.com/api/v2/list/${NEWSLETTER_LIST_ID}/subscribe?api_key=${process.env.KLAVIYO_PRIVATE_API_KEY}`;
     const response = await axios.post(url, req.body);
     return res.status(200).json(response.data);
--- src/components/SignUpBlock/index.jsx
+++ src/components/SignUpBlock/index.jsx
@@ -1,11 +1,51 @@
 import React, { useState } from "react";
 import { WidthContainer } from "~components";
+import regex from "~constants/regex";
 import * as styles from "./SignUpBlock.module.scss";
 
 const SignUpBlock = () => {
   const [inputValue, setInputValue] = useState(``);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const [hasApiError, setHasApiError] = useState(false);
+  const [hasValidationError, setHasValidationError] = useState(false);
 
-  const handleChange = (e) => setInputValue(e.target.value);
+  const handleChange = (e) => {
+    if (hasSubmitted) return;
+    setInputValue(e.target.value);
+    setHasValidationError(false);
+    setHasApiError(false);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (hasSubmitted || isSubmitting) return;
+    if (!regex.email.test(inputValue)) {
+      setHasValidationError(true);
+    } else {
+      setIsSubmitting(true);
+      try {
+        const response = await fetch(`/api/subscribe-to-newsletter`, {
+          method: `POST`,
+          headers: {
+            "content-type": `application/json`
+          },
+          body: JSON.stringify({
+            profiles: [{ email: inputValue }]
+          })
+        });
+        const data = await response.json();
+        setIsSubmitting(false);
+        if (data.statusCode === 500) {
+          throw new Error(data.body.code);
+        }
+        setHasSubmitted(true);
+      } catch (err) {
+        console.error(err);
+        setHasApiError(true);
+      }
+    }
+  };
 
   return (
     <div className={styles.container} id="sign-up-block">
@@ -21,19 +61,22 @@ const SignUpBlock = () => {
             We’ll send you the lates information on new products, events and
             meet ups before anyone else.
           </p>
-          <form className={styles.form}>
+          <form onSubmit={handleSubmit}>
             <input
               value={inputValue}
               onChange={handleChange}
               type="text"
-              className={styles.input}
+              className={[
+                styles.input,
+                hasValidationError ? styles.error : null
+              ].join(` `)}
               placeholder="Your journey stars here."
             />
             <button
               type="submit"
               className={[`button-text`, styles.signUpButton].join(` `)}
             >
-              Sign up
+              {hasSubmitted ? `Thanks` : `Sign up`}
             </button>
           </form>
         </div>
