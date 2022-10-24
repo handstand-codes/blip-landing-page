be64ea5d20ebdac2d0b3ee87866baf299d64ca9e
flangham <finnian.langham@gmail.com>
2022-10-24 11:53:24 +1100

    Created 404 page

---
+++ src/pages/404/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "gatsby";
+import { SEO, FillAvailableHeight, HeroLogo } from "~components";
+import * as styles from "./404.module.scss";
+
+const FourOhFour = () => (
+  <FillAvailableHeight>
+    <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <HeroLogo />
+      </div>
+      <h2 className={[`h1`, styles.header].join(` `)}>
+        Sorry, this page doesn‘t exist.
+      </h2>
+      <Link className={[`b1`, styles.homeLink].join(` `)} to="/">
+        Back to home
+      </Link>
+    </div>
+  </FillAvailableHeight>
+);
+
+export default FourOhFour;
+
+export const Head = () => <SEO />;
