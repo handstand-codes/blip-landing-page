f5e6629d809eb4b1bc970abf02ed7e8d1b30d14e
flangham <finnian.langham@gmail.com>
2022-10-24 11:39:27 +1100

    Use native browser validation

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import regex from "~constants/regex";
 import * as styles from "./SignUp.module.scss";
 
 const SignUp = () => {
@@ -7,66 +6,74 @@ const SignUp = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [hasSubmitted, setHasSubmitted] = useState(false);
   const [hasApiError, setHasApiError] = useState(false);
-  const [hasValidationError, setHasValidationError] = useState(false);
 
   const handleChange = (e) => {
     if (hasSubmitted) return;
     setInputValue(e.target.value);
-    setHasValidationError(false);
     setHasApiError(false);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (hasSubmitted || isSubmitting) return;
-    if (!regex.email.test(inputValue)) {
-      setHasValidationError(true);
-    } else {
-      setIsSubmitting(true);
-      try {
-        const response = await fetch(`/api/subscribe-to-newsletter`, {
-          method: `POST`,
-          headers: {
-            "content-type": `application/json`
-          },
-          body: JSON.stringify({
-            profiles: [{ email: inputValue }]
-          })
-        });
-        const data = await response.json();
-        setIsSubmitting(false);
-        if (data.statusCode === 500) {
-          throw new Error(data.body.code);
-        }
-        setHasSubmitted(true);
-      } catch (err) {
-        console.error(err);
-        setHasApiError(true);
+
+    setIsSubmitting(true);
+    try {
+      const response = await fetch(`/api/subscribe-to-newsletter`, {
+        method: `POST`,
+        headers: {
+          "content-type": `application/json`
+        },
+        body: JSON.stringify({
+          profiles: [{ email: inputValue }]
+        })
+      });
+      const data = await response.json();
+      setIsSubmitting(false);
+      if (data.statusCode === 500) {
+        throw new Error(data.body.code);
       }
+      setHasSubmitted(true);
+    } catch (err) {
+      console.error(err);
+      setHasApiError(true);
     }
   };
 
   return (
-    <div className={styles.container} id="sign-up-block">
+    <div
+      className={[
+        styles.container,
+        isSubmitting ? styles.submitting : null,
+        hasSubmitted ? styles.submitted : null,
+        hasApiError ? styles.apiError : null
+      ].join(` `)}
+    >
       <form className={styles.form} onSubmit={handleSubmit}>
         <input
           value={inputValue}
           onChange={handleChange}
-          type="text"
-          className={[
-            `b1`,
-            styles.input,
-            hasValidationError ? styles.error : null
-          ].join(` `)}
+          type="email"
+          maxLength={100}
+          className={[`b1`, styles.input].join(` `)}
           placeholder="Your Email"
         />
         <button
           type="submit"
           className={[`button-text`, styles.signUpButton].join(` `)}
         >
-          {hasSubmitted ? `Thanks` : `Sign up`}
+          <span className={styles.buttonText}>Sign up</span>
+          <div className={styles.spinnerContainer}>
+            <div className={styles.spinner} />
+          </div>
         </button>
       </form>
+      <p className={[`b1`, styles.successMessage].join(` `)}>
+        Thanks for signing up! Your invite is on it&apos;s way.
+      </p>
+      <p className={[`b1`, styles.errorMessage].join(` `)}>
+        We&apos;re sorry, something went wrong! Please try again later.
+      </p>
     </div>
   );
 };
